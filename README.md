b2b060df7caa9366b9308e181d57bcf842b78727
Tomas Jelen <tomas@delikates.org>
2016-05-09 21:42:34 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,9 @@
+## About
+
+The official `postgres` docker with Czech full-text search dictionaries pulled from https://postgres.cz/data/czech.tar.gz.
+
+## Supported tags
+
+* `9.4`
+
+    Based on `postgres:9.4`.
---
+++ add-tsearch-czech.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+  create text search dictionary czech_spell
+    (template=ispell, dictfile=czech, afffile=czech, stopwords=czech);
+
+  create text search configuration czech (copy=english);
+
+  alter text search configuration czech
+    alter mapping for word, asciiword with czech_spell, simple;
+EOSQL
